6ad99b3b83d67a7011aff74540eed53c0736144a
6A <78461864+6A-Realm@users.noreply.github.com>
2021-12-29 12:29:08 -0800

    Discord updates

--- cogs/friends.py
+++ cogs/friends.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import json
     
@@ -6,47 +7,150 @@ class friend(commands.Cog):
         self.client = client
 
 # {-- Public Commands --}
-    @commands.command()
+    @commands.group(invoke_without_command=True)
     @commands.guild_only()
-    async def addcode(self, ctx, *, fc): 
-        with open('res/friendcodes.json', 'r') as f: 
+    async def addcode(self, ctx): 
+        await ctx.send("Use the command addcode <version> <fcode> to enter your friend code. Versions are switch, ds, and home.")
+
+    @addcode.group()
+    @commands.guild_only()
+    async def switch(self, ctx, *, fc): 
+        with open('res/friendcodes-switch.json', 'r') as f: 
             codes = json.load(f) 
         if str(ctx.message.author.id) in codes:
-            await ctx.send("Use the `changecode` command to edit your friend-code.")
+            await ctx.send("Use the `change switch` command to edit your friend-code.")
         else:
             codes[str(ctx.message.author.id)] = fc
-            with open('res/friendcodes.json', 'w') as f: 
+            with open('res/friendcodes-switch.json', 'w') as f: 
                 json.dump(codes, f, indent=4) 
             await ctx.send(f"`{fc}` added.")
 
-    @commands.command()
-    async def fcode(self, ctx): 
-        with open('res/friendcodes.json', 'r') as f:
-            codes = json.load(f)
-        if str(ctx.message.author.id) in codes:
-            await ctx.send(f"Your friend code is `{codes[str(ctx.message.author.id)]}`.")
-        else: 
-            await ctx.send("Add your friend-code using `addcode`.")
-
-    @commands.command()
+    @addcode.group()
     @commands.guild_only()
-    async def changecode(self, ctx, *, fc): 
-        with open('res/friendcodes.json', 'r') as f:
+    async def ds(self, ctx, *, fc): 
+        with open('res/friendcodes-ds.json', 'r') as f: 
+            codes = json.load(f) 
+        if str(ctx.message.author.id) in codes:
+            await ctx.send("Use the `change ds` command to edit your friend-code.")
+        else:
+            codes[str(ctx.message.author.id)] = fc
+            with open('res/friendcodes-ds.json', 'w') as f: 
+                json.dump(codes, f, indent=4) 
+            await ctx.send(f"`{fc}` added.")
+
+    @addcode.group()
+    @commands.guild_only()
+    async def home(self, ctx, *, fc): 
+        with open('res/friendcodes-home.json', 'r') as f: 
+            codes = json.load(f) 
+        if str(ctx.message.author.id) in codes:
+            await ctx.send("Use the `change home` command to edit your friend-code.")
+        else:
+            codes[str(ctx.message.author.id)] = fc
+            with open('res/friendcodes-home.json', 'w') as f: 
+                json.dump(codes, f, indent=4) 
+            await ctx.send(f"`{fc}` added.")
+
+    @commands.group(invoke_without_command=True)
+    @commands.guild_only()
+    async def change(self, ctx): 
+        await ctx.send("Use the command change <version> <fcode> to enter your friend code. Versions are switch, ds, and home.")
+
+    @change.group()
+    @commands.guild_only()
+    async def switch(self, ctx, *, fc): 
+        with open('res/friendcodes-switch.json', 'r') as f:
             codes = json.load(f)
         codes[str(ctx.message.author.id)] = fc
-        with open('res/friendcodes.json', 'w') as f: 
+        with open('res/friendcodes-switch.json', 'w') as f: 
             json.dump(codes, f, indent=4)
         await ctx.send(f'Your friend-code has been changed to: `{fc}`') 
 
-    @commands.command()
+    @change.group()
+    @commands.guild_only()
+    async def ds(self, ctx, *, fc): 
+        with open('res/friendcodes-ds.json', 'r') as f:
+            codes = json.load(f)
+        codes[str(ctx.message.author.id)] = fc
+        with open('res/friendcodes-ds.json', 'w') as f: 
+            json.dump(codes, f, indent=4)
+        await ctx.send(f'Your friend-code has been changed to: `{fc}`') 
+
+    @change.group()
+    @commands.guild_only()
+    async def home(self, ctx, *, fc): 
+        with open('res/friendcodes-home.json', 'r') as f:
+            codes = json.load(f)
+        codes[str(ctx.message.author.id)] = fc
+        with open('res/friendcodes-home.json', 'w') as f: 
+            json.dump(codes, f, indent=4)
+        await ctx.send(f'Your friend-code has been changed to: `{fc}`') 
+
+    @commands.group(invoke_without_command=True)
     @commands.guild_only()
     async def removecode(self, ctx): 
-        with open('res/friendcodes.json', 'r') as f:
+        await ctx.send("Use the command removecode <version> delete your friend code. Versions are switch, ds, and home.")
+
+    @removecode.group()
+    @commands.guild_only()
+    async def switch(self, ctx): 
+        try:
+            with open('res/friendcodes-switch.json', 'r') as f:
+                codes = json.load(f)
+            codes.pop(str(ctx.message.author.id))
+            with open('res/friendcodes-switch.json', 'w') as f:
+                json.dump(codes, f, indent=4)
+            await ctx.send("Your friend-code has been removed.")
+        except:
+            await ctx.send("You do not have a friend-code for this game version.")
+
+    @removecode.group()
+    @commands.guild_only()
+    async def ds(self, ctx): 
+        try:
+            with open('res/friendcodes-ds.json', 'r') as f:
+                codes = json.load(f)
+            codes.pop(str(ctx.message.author.id))
+            with open('res/friendcodes-ds.json', 'w') as f:
+                json.dump(codes, f, indent=4)
+            await ctx.send("Your friend-code has been removed.")
+        except:
+            await ctx.send("You do not have a friend-code for this game version.")
+
+    @removecode.group()
+    @commands.guild_only()
+    async def home(self, ctx): 
+        try:
+            with open('res/friendcodes-home.json', 'r') as f:
+                codes = json.load(f)
+            codes.pop(str(ctx.message.author.id))
+            with open('res/friendcodes-home.json', 'w') as f:
+                json.dump(codes, f, indent=4)
+            await ctx.send("Your friend-code has been removed.")
+        except:
+            await ctx.send("You do not have a friend-code for this game version.")
+
+    @commands.command()
+    async def fcode(self, ctx):
+        embed=discord.Embed(title=f'Your friend codes', description="Your saved friend codes are listed below.", color=ctx.author.color)
+        with open('res/friendcodes-switch.json', 'r') as f:
             codes = json.load(f)
-        codes.pop(str(ctx.message.author.id))
-        with open('res/friendcodes.json', 'w') as f:
-            json.dump(codes, f, indent=4)
-        await ctx.send("Your friend-code has been removed.")
+        if str(ctx.message.author.id) in codes:
+            embed.add_field(name="Switch:", value={codes[str(ctx.message.author.id)]}, inline=False)
+        else: 
+            pass
+        with open('res/friendcodes-ds.json', 'r') as f:
+            codes = json.load(f)
+        if str(ctx.message.author.id) in codes:
+            embed.add_field(name="DS::", value={codes[str(ctx.message.author.id)]}, inline=False)
+        else: 
+            pass        
+        with open('res/friendcodes-home.json', 'r') as f:
+            codes = json.load(f)
+        if str(ctx.message.author.id) in codes:
+            embed.add_field(name="Home:", value={codes[str(ctx.message.author.id)]}, inline=False)
+        else: 
+            pass
 
 
 def setup(client):
--- cogs/votelock.py
+++ cogs/votelock.py
@@ -1,41 +1,53 @@
 import discord
-from discord import client
 from discord.ext import commands
-import asyncio
 
+voted = []
+votes = []
 
 class votelock(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    client.lan = 0
 
 # {-- Public Commands --}
     @commands.command()
-    @commands.cooldown(1, 3600, commands.BucketType.user)
     async def votelock(self, ctx):
-        client.lan += 1
-        if client.lan == 1:
-            await ctx.send(f'{ctx.message.author} has voted to lock this channel. 2 more votes are required.')
-        if client.lan == 2: 
-            await ctx.send(f'{ctx.message.author} has voted to lock this channel. 1 more vote is required.')
-        if client.lan == 3: 
-            await ctx.send(f'{ctx.message.author} has voted to lock this channel.')
+        user = ctx.message.author.id
+        channel = ctx.message.channel.id
+        name = ctx.message.author.name
+        counter = votes.count(channel)
+
+        if ctx.message.author.id in voted:
+            return await ctx.send("You've already voted to lock down a channel. {}/3 votes have been recorded.").format(votes.count(channel))
+
+        voted.append(user)
+        votes.append(channel)
+
+        if counter <3:
+            await ctx.message.delete()
+            await ctx.send("{} voted to lock this channel. {}/3 votes have been recorded.".format(name, votes.count(channel)))
+        if counter >=3:
+            await ctx.message.delete()
             overwrite = ctx.channel.overwrites_for(ctx.guild.default_role)
             overwrite.send_messages = False
             await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
             await ctx.send(':red_circle: **Channel locked.** This bot is offline, please use one of the other bots.')
-            if ctx.channel.id == 860765383908655144:
-                down = discord.Embed(title="LGPE Bot Is Down!", description=f"Many users have been experiencing bot disconnection, meaning that they cannot complete their trades. This means the bot has crashed; therefore, this channel has been locked.\n\n\n**Do not DM anyone about the bot. Do not ask when the bot will be up.**\n\n\nRemember, this is a free service that nobody else offers. This is also something that the other sysbot creators said would be impossible to make.\n\n\nThank you for waiting patiently.", colour=discord.Colour.orange())
-                down.set_footer(text="Failure to follow bolded line will result in a ban.")  
-                await ctx.send(embed = down)
-            client.lan = 0
+            down = discord.Embed(title="Bot Is Down!", description=f"Many users have been experiencing bot disconnection, meaning that they cannot complete their trades. This means the bot has crashed; therefore, this channel has been locked.\n\n\n**Do not DM anyone about the bot. Do not ask when the bot will be up.**\n\n\nRemember, this is a free service that nobody else offers. This is also something that the other sysbot creators said would be impossible to make.\n\n\nThank you for waiting patiently.", colour=discord.Colour.orange())
+            down.set_footer(text="Failure to follow bolded line will result in a ban.")  
+            await ctx.send(embed = down)
+            while(True):
+                if votes != 0 or voted != 0:
+                    votes.clear()
+                    voted.clear()
+                else:
+                    return
 
 # {-- Admin Commands --}
     @commands.command()
     @commands.has_permissions(manage_channels=True)
     async def clearvotes(self, ctx):
-        client.lan = 0
+        voted.clear()
+        votes.clear()
         await ctx.send('Votes have reset to 0.')
 
 
